ff3c849ec7ce21f689b5d1d3fd9f5e82f21c08f7
すずねーう <suzuneu@suzuneu.com>
2023-07-16 12:27:13 +0000

    ✨ feat: add ESLint and Prettier configurations

---
+++ .eslintrc.cjs
@@ -0,0 +1,42 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es2021": true
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended"
+  ],
+  "overrides": [
+    {
+      "env": {
+        "node": true
+      },
+      "files": [
+        ".eslintrc.{js,cjs}"
+      ],
+      "parserOptions": {
+        "sourceType": "script"
+      }
+    },
+    {
+      "env": {
+        "node": true
+      },
+      "files": [
+        "build.js"
+      ]
+    }
+  ],
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "ecmaVersion": "latest",
+    "sourceType": "module"
+  },
+  "plugins": [
+    "@typescript-eslint"
+  ],
+  "rules": {
+    "@typescript-eslint/no-unused-vars": ["warn"]
+  }
+}
